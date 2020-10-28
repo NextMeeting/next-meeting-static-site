289363123711272dc0f187876d6e694ee2cac890
Michoel Samuels <k2co3@icloud.com>
2020-10-28 11:57:50 +0200

    Complete basic styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Imports
 
-const { src, watch, dest, series, parallel } = require('gulp');
+const { src, watch, symlink, dest, series, parallel } = require('gulp');
 
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
@@ -18,28 +18,52 @@ const clean = require('gulp-clean');
 
 */
 
+// Fonts
+// Production: <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
-const defaultTask = series(
-  removePreviousBuild,
-  buildCss,
-  buildHtml
-)
+
+const defaultTask = function() {
+  watch('src/*', series(
+      removePreviousBuild,
+      buildCss,
+      buildHtml
+    )
+  );
+}
 
 function removePreviousBuild() {
-  return src('tmp', {read: false})
+  return src('tmp', {read: false, allowEmpty: true})
     .pipe(clean());
 }
 
+
 async function buildHtml() {
+  
+  // Symlink the prebuild Tailwind file into /tmp #perfmatters
+  src('dev_static/tailwind_full.css')
+    .pipe(symlink('tmp/'));
+
+  src("node_modules/alpinejs/dist/alpine.js")
+    .pipe(symlink('tmp/'));
+  
+    src("node_modules/clipboard/dist/clipboard.js")
+    .pipe(symlink('tmp/'));
+
+  // Inject a link to the stylesheet into the HTML
   src('src/index.html').
-  pipe(replace("<!-- TAILWIND_DEV -->", "<link rel=\"stylesheet\" src=\"../dev_static/tailwind_full.css\">")).
-  pipe(dest('tmp'));
+    pipe(replace("<!-- TAILWIND_DEV -->", "<link rel=\"stylesheet\" href=\"tailwind_full.css\">")).
+    pipe(replace("<!-- JS_DEV -->", `<script src=\"alpine.js\"></script><script src=\"clipboard.js\"></script>`)).
+    pipe(replace("<!-- FONTS -->", `<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">`)).
+    pipe(replace("<!-- CSS -->", `<link rel="stylesheet" href="styles.css">`)).
+    pipe(dest('tmp'));
 }
+
 
 async function buildCss (cb) {
   return src('src/styles.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('tmp/styles.css'));
+    .pipe(dest('tmp'));
 }
+
 
 exports.default = defaultTask
